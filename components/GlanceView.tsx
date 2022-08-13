186a9147696b3275592a31852ce19ce759297659
sujen <sujenphea@gmail.com>
2022-08-13 16:35:50 +1200

    fix: mobile responsive glance view

--- components/GlanceView.tsx
+++ components/GlanceView.tsx
@@ -1,3 +1,5 @@
+import { css } from '@emotion/react'
+
 import ProjectType from '../types/projectType'
 
 import projectsJSON from '../data/projects.json'
@@ -7,42 +9,51 @@ import worksJSON from '../data/works.json'
 
 // display each project
 const ProjectView = (props: { project: ProjectType }) => {
+  // styles
+  const styles = {
+    containerCss: css`
+      position: relative;
+      width: 100%;
+      height: 200px;
+      padding-left: 80px;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: start;
+      align-items: start;
+
+      overflow-y: scroll;
+      overflow-x: hidden;
+
+      text-align: start;
+
+      @media (min-width: 768px) {
+        flex-direction: row;
+        align-items: center;
+      }
+    `,
+    columnCss: css`
+      display: flex;
+      flex-direction: column;
+      justify-content: left;
+      align-items: left;
+
+      @media (min-width: 768px) {
+        padding-left: 40px;
+      }
+    `,
+    titleCss: css`
+      text-transform: uppercase;
+      font-size: 24px;
+      font-size: calc(100% + 1.5vw + 1vh);
+    `,
+  }
+
   return (
-    <div
-      style={{
-        position: 'relative',
-        width: '100vw',
-        height: '150px',
-
-        overflowY: 'scroll',
-
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'left',
-        alignItems: 'center',
-      }}
-    >
-      <div
-        style={{
-          padding: '0 20px',
-
-          fontSize: '38px',
-          textTransform: 'uppercase',
-        }}
-      >
-        {props.project.name}
-      </div>
+    <div css={styles.containerCss}>
+      <div css={styles.titleCss}>{props.project.name}</div>
       {/* column */}
-      <div
-        style={{
-          padding: '0px 20px',
-
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'left',
-          alignItems: 'left',
-        }}
-      >
+      <div css={styles.columnCss}>
         <div>{props.project.company}</div>
         <div>{props.project.technologies}</div>
         <div>{props.project.year}</div>
@@ -52,32 +63,48 @@ const ProjectView = (props: { project: ProjectType }) => {
 }
 
 const GlanceView = (props: { visible: boolean }) => {
+  // styles
+  const styles = {
+    containerCss: css`
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+
+      display: ${props.visible ? `block` : `none`};
+      width: 100%;
+      padding-top: 40px;
+
+      zindex: 1;
+      overflow-y: scroll;
+      overflow-x: hidden;
+
+      text-align: center;
+      background-color: transparent;
+      color: white;
+
+      @media (min-width: 768px) {
+        text-align: start;
+      }
+    `,
+    titleCss: css`
+      font-size: 32px;
+      font-size: calc(100% + 1.5vw + 1.5vh);
+
+      @media (min-width: 768px) {
+        padding-left: 80px;
+      }
+    `,
+  }
+
   return (
-    <div
-      style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        display: props.visible ? 'block' : 'none',
-
-        zIndex: 1,
-        overflowY: 'scroll',
-
-        width: '100vw',
-        paddingTop: '40px',
-        paddingLeft: '40px',
-
-        backgroundColor: 'transparent',
-        color: 'white',
-      }}
-    >
-      <h1>Projects</h1>
+    <div css={styles.containerCss}>
+      <h1 css={styles.titleCss}>Projects</h1>
       {projectsJSON.map((project, i) => (
         <ProjectView project={project} key={i} />
       ))}
-      <h1>Works</h1>
+      <h1 css={styles.titleCss}>Works</h1>
       {worksJSON.map((project, i) => (
         <ProjectView project={project} key={i} />
       ))}
