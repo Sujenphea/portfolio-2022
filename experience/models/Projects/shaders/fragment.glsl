da1a3f10e434363aa17d9de5bde16f0e24fce01f
sujen <sujenphea@gmail.com>
2022-08-31 06:28:14 +1200

    fix: project invisible in canvas if far away
    - change to power of 3 to maintain negative value

--- experience/models/Projects/shaders/fragment.glsl
+++ experience/models/Projects/shaders/fragment.glsl
@@ -8,7 +8,7 @@ void main() {
     vec4 textureColor = texture2D(uTexture, vUv);
 
     // distance between camera, project = opacity of project
-    float distanceFromProject = clamp(pow(1.2 - abs(uDistance), 2.0), 0.0, 1.0);
+    float distanceFromProject = clamp(pow(1.2 - abs(uDistance), 3.0), 0.0, 1.0);
 
     gl_FragColor = vec4(textureColor.xyz, uOpacity * distanceFromProject);
 }
